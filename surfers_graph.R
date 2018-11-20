9ba3f68a65dad89b3214ad644bd47d3bf6509e05
Tavo <gariosp@unal.edu.co>
2018-11-20 20:46:45 -0300

    Adding data set and splitting observations and perceptions

--- surfers_graph.R
+++ surfers_graph.R
@@ -9,3 +9,10 @@ library(corrplot)
 
 rm(list=ls())
 setwd("E:/UBA/2018-II/DM en Ciencia y Tecnología/Ciclos Sueño")
+
+
+install.packages("igraph");
+
+download.file("http://moreno.ss.uci.edu/beach.dat", destfile = "windsurfers.dat")
+ws <- read.table("windsurfers.dat", skip = 7)
+dim(ws)
