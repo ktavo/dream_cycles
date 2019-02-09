a5a7b059edb49b25c895a9a261c887be8f6ed238
Tavo <gariosp@unal.edu.co>
2019-02-09 20:57:03 -0300

    Spliting mean for participans on generacionDatos.R

--- tpneuro.r
+++ tpneuro.r
@@ -10,123 +10,12 @@ library(igraph)
 
 library(corrplot)
 
-########################Generación Redes Promedio########################
 N1 <- read.csv("N1promedio.csv",header=FALSE)
 N2 <- read.csv("N2promedio.csv",header=FALSE)
 N3 <- read.csv("N3promedio.csv",header=FALSE)
 W <- read.csv("wpromedio.csv",header=FALSE)
 
 
-
-
-#############################W#############################
-W_suj1 <- read.csv("DataSujetos/W_suj1.csv",header=FALSE)
-W_suj2 <- read.csv("DataSujetos/W_suj2.csv",header=FALSE)
-W_suj3 <- read.csv("DataSujetos/W_suj3.csv",header=FALSE)
-W_suj4 <- read.csv("DataSujetos/W_suj4.csv",header=FALSE)
-W_suj5 <- read.csv("DataSujetos/W_suj5.csv",header=FALSE)
-W_suj6 <- read.csv("DataSujetos/W_suj6.csv",header=FALSE)
-W_suj7 <- read.csv("DataSujetos/W_suj7.csv",header=FALSE)
-W_suj8 <- read.csv("DataSujetos/W_suj8.csv",header=FALSE)
-W_suj9 <- read.csv("DataSujetos/W_suj9.csv",header=FALSE)
-W_suj10 <- read.csv("DataSujetos/W_suj10.csv",header=FALSE)
-W_suj11 <- read.csv("DataSujetos/W_suj11.csv",header=FALSE)
-W_suj12 <- read.csv("DataSujetos/W_suj12.csv",header=FALSE)
-W_suj13 <- read.csv("DataSujetos/W_suj13.csv",header=FALSE)
-W_suj14 <- read.csv("DataSujetos/W_suj14.csv",header=FALSE)
-W_suj15 <- read.csv("DataSujetos/W_suj15.csv",header=FALSE)
-W_suj16 <- read.csv("DataSujetos/W_suj16.csv",header=FALSE)
-W_suj17 <- read.csv("DataSujetos/W_suj17.csv",header=FALSE)
-W_suj18 <- read.csv("DataSujetos/W_suj18.csv",header=FALSE)
-#############################END W#############################
-
-#############################N1#############################
-N1_suj1 <- read.csv("DataSujetos/N1_suj1.csv",header=FALSE)
-N1_suj2 <- read.csv("DataSujetos/N1_suj2.csv",header=FALSE)
-N1_suj3 <- read.csv("DataSujetos/N1_suj3.csv",header=FALSE)
-N1_suj4 <- read.csv("DataSujetos/N1_suj4.csv",header=FALSE)
-N1_suj5 <- read.csv("DataSujetos/N1_suj5.csv",header=FALSE)
-N1_suj6 <- read.csv("DataSujetos/N1_suj6.csv",header=FALSE)
-N1_suj7 <- read.csv("DataSujetos/N1_suj7.csv",header=FALSE)
-N1_suj8 <- read.csv("DataSujetos/N1_suj8.csv",header=FALSE)
-N1_suj9 <- read.csv("DataSujetos/N1_suj9.csv",header=FALSE)
-N1_suj10 <- read.csv("DataSujetos/N1_suj10.csv",header=FALSE)
-N1_suj11 <- read.csv("DataSujetos/N1_suj11.csv",header=FALSE)
-N1_suj12 <- read.csv("DataSujetos/N1_suj12.csv",header=FALSE)
-N1_suj13 <- read.csv("DataSujetos/N1_suj13.csv",header=FALSE)
-N1_suj14 <- read.csv("DataSujetos/N1_suj14.csv",header=FALSE)
-N1_suj15 <- read.csv("DataSujetos/N1_suj15.csv",header=FALSE)
-N1_suj16 <- read.csv("DataSujetos/N1_suj16.csv",header=FALSE)
-N1_suj17 <- read.csv("DataSujetos/N1_suj17.csv",header=FALSE)
-N1_suj18 <- read.csv("DataSujetos/N1_suj18.csv",header=FALSE)
-#############################END N1#############################
-
-#############################N2#############################
-N2_suj1 <- read.csv("DataSujetos/N2_suj1.csv",header=FALSE)
-N2_suj2 <- read.csv("DataSujetos/N2_suj2.csv",header=FALSE)
-N2_suj3 <- read.csv("DataSujetos/N2_suj3.csv",header=FALSE)
-N2_suj4 <- read.csv("DataSujetos/N2_suj4.csv",header=FALSE)
-N2_suj5 <- read.csv("DataSujetos/N2_suj5.csv",header=FALSE)
-N2_suj6 <- read.csv("DataSujetos/N2_suj6.csv",header=FALSE)
-N2_suj7 <- read.csv("DataSujetos/N2_suj7.csv",header=FALSE)
-N2_suj8 <- read.csv("DataSujetos/N2_suj8.csv",header=FALSE)
-N2_suj9 <- read.csv("DataSujetos/N2_suj9.csv",header=FALSE)
-N2_suj10 <- read.csv("DataSujetos/N2_suj10.csv",header=FALSE)
-N2_suj11 <- read.csv("DataSujetos/N2_suj11.csv",header=FALSE)
-N2_suj12 <- read.csv("DataSujetos/N2_suj12.csv",header=FALSE)
-N2_suj13 <- read.csv("DataSujetos/N2_suj13.csv",header=FALSE)
-N2_suj14 <- read.csv("DataSujetos/N2_suj14.csv",header=FALSE)
-N2_suj15 <- read.csv("DataSujetos/N2_suj15.csv",header=FALSE)
-N2_suj16 <- read.csv("DataSujetos/N2_suj16.csv",header=FALSE)
-N2_suj17 <- read.csv("DataSujetos/N2_suj17.csv",header=FALSE)
-N2_suj18 <- read.csv("DataSujetos/N2_suj18.csv",header=FALSE)
-#############################END N2#############################
-
-#############################N3#############################
-N3_suj1 <- read.csv("DataSujetos/N3_suj1.csv",header=FALSE)
-N3_suj2 <- read.csv("DataSujetos/N3_suj2.csv",header=FALSE)
-N3_suj3 <- read.csv("DataSujetos/N3_suj3.csv",header=FALSE)
-N3_suj4 <- read.csv("DataSujetos/N3_suj4.csv",header=FALSE)
-N3_suj5 <- read.csv("DataSujetos/N3_suj5.csv",header=FALSE)
-N3_suj6 <- read.csv("DataSujetos/N3_suj6.csv",header=FALSE)
-N3_suj7 <- read.csv("DataSujetos/N3_suj7.csv",header=FALSE)
-N3_suj8 <- read.csv("DataSujetos/N3_suj8.csv",header=FALSE)
-N3_suj9 <- read.csv("DataSujetos/N3_suj9.csv",header=FALSE)
-N3_suj10 <- read.csv("DataSujetos/N3_suj10.csv",header=FALSE)
-N3_suj11 <- read.csv("DataSujetos/N3_suj11.csv",header=FALSE)
-N3_suj12 <- read.csv("DataSujetos/N3_suj12.csv",header=FALSE)
-N3_suj13 <- read.csv("DataSujetos/N3_suj13.csv",header=FALSE)
-N3_suj14 <- read.csv("DataSujetos/N3_suj14.csv",header=FALSE)
-N3_suj15 <- read.csv("DataSujetos/N3_suj15.csv",header=FALSE)
-N3_suj16 <- read.csv("DataSujetos/N3_suj16.csv",header=FALSE)
-N3_suj17 <- read.csv("DataSujetos/N3_suj17.csv",header=FALSE)
-N3_suj18 <- read.csv("DataSujetos/N3_suj18.csv",header=FALSE)
-#############################END N2#############################
-
-mean_maker <- function(suj1, suj2, suj3, suj4, suj5, suj6, suj7, suj8, suj9,suj10, suj11, suj12, suj13, suj14, suj15, suj16,suj17, suj18) {
-  mean_result <- (suj1 + suj2 + suj3 + suj4 + suj5 + suj6 + suj7 + suj8 + suj9 + suj10 + suj11 + suj12 + suj13 + suj14 + suj15 + suj16 + suj17 + suj18)/18
-  return(mean_result)
-}
-
-#N1_suj1 <- as.matrix(N1_suj1)
-#N1_suj2 <- as.matrix(N1_suj2)
-
-
-#mean_vector <- c(N1_suj1,N1_suj2)
-
-generated_W <- mean_maker(W_suj1, W_suj2, W_suj3, W_suj4, W_suj5, W_suj6, W_suj7, W_suj8, W_suj9, W_suj10, W_suj11, W_suj12, W_suj13, W_suj14, W_suj15, W_suj16, W_suj17, W_suj18)
-generated_W <-  round(generated_W,digits=6)
-generated_N1 <- mean_maker(N1_suj1, N1_suj2, N1_suj3, N1_suj4, N1_suj5, N1_suj6, N1_suj7, N1_suj8, N1_suj9, N1_suj10, N1_suj11, N1_suj12, N1_suj13, N1_suj14, N1_suj15, N1_suj16, N1_suj17, N1_suj18)
-generated_N1 <-  round(generated_N1,digits=6)
-generated_N2 <- mean_maker(N2_suj1, N2_suj2, N2_suj3, N2_suj4, N2_suj5, N2_suj6, N2_suj7, N2_suj8, N2_suj9, N2_suj10, N2_suj11, N2_suj12, N2_suj13, N2_suj14, N2_suj15, N2_suj16, N2_suj17, N2_suj18)
-generated_N2 <-  round(generated_N2,digits=6)
-generated_N3 <- mean_maker(N3_suj1, N3_suj2, N3_suj3, N3_suj4, N3_suj5, N3_suj6, N3_suj7, N3_suj8, N3_suj9, N3_suj10, N3_suj11, N3_suj12, N3_suj13, N3_suj14, N3_suj15, N3_suj16, N3_suj17, N3_suj18)
-generated_N3 <-  round(generated_N3,digits=6)
-
-write.csv(generated_W, file = "Wpromedio.csv")
-write.csv(generated_N2, file = "N2promedio.csv")
-write.csv(generated_N3, file = "N3promedio.csv")
-########################FIN Generación Redes Promedio########################
 
 W <- read.csv("Wpromedio.csv",header=FALSE)
 N1 <- read.csv("N1promedio.csv",header=FALSE)
@@ -180,15 +69,20 @@ corrplot(N3, is.corr=TRUE, title = "N3")#, order="hclust")
 N = dim(N1)[1]
 Nmaxlinks = N*(N-1)
 ##############################Delta calculation##############################
-n = 1000
+n = 700
 delta = n/Nmaxlinks
 print(delta)
+
+n = Nmaxlinks*delta
+
+delta = 0.038
 ##############################Delta calculation##############################
 
 diag(W)<-0
 tmp.W<-sort(as.vector(W),decreasing = TRUE)
 ro.W = tmp.W[n]
 Wb.W = (W>ro.W)
+
 
 diag(N1)<-0
 tmp.N1<-sort(as.vector(N1),decreasing = TRUE)
